a31076a50d74c17b4a85395c0ea15b745692e599
hainet <20400616+hainet@users.noreply.github.com>
2018-02-01 23:24:21 +0900

    BeforeTransaction, AfterTransactionのサンプル

--- src/main/java/com/hainet/spring/test/sample/SpringTestSampleApplication.java
+++ src/main/java/com/hainet/spring/test/sample/SpringTestSampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringTestSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTestSampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestSampleApplication.class, args);
+    }
 }
---
+++ src/test/java/com/hainet/spring/test/sample/BeforeAfterTransactionTest.java
@@ -0,0 +1,35 @@
+package com.hainet.spring.test.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.BeforeTransaction;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeforeAfterTransactionTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeTransaction
+    public void beforeTransaction() {
+        System.out.println("This is before transaction.");
+    }
+
+    @AfterTransaction
+    public void afterTransaction() {
+        System.out.println("This is after transaction.");
+    }
+
+    @Test
+    @Transactional
+    public void beforeTransactionTest() {
+        jdbcTemplate.queryForList("SELECT id, name FROM person");
+    }
+}
