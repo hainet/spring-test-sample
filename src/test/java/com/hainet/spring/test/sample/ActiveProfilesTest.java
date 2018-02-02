8b0a751962543737f713536ebd807a47836b7255
hainet <20400616+hainet@users.noreply.github.com>
2018-02-02 23:04:40 +0900

    ActiveProfilesのサンプル

---
+++ src/test/java/com/hainet/spring/test/sample/ActiveProfilesTest.java
@@ -0,0 +1,25 @@
+package com.hainet.spring.test.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("prod")
+public class ActiveProfilesTest {
+
+    @Value("${env}")
+    private String env;
+
+    @Test
+    public void activeProfilesTest() {
+        assertThat(env, is("prod"));
+    }
+}
